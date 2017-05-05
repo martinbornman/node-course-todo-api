f9190752c52380c057edf654735462d5ae8fdd47
Martin Bornman <martinbornman@Martins-MacBook-Pro.local>
2017-05-05 08:33:55 +0200

    Add GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -41,6 +41,7 @@ describe('POST /todos', () => {
                 if(err) {
                     return done(err);
                 }
+                
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(0);
                     done();
